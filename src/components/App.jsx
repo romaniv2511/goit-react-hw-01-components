62ae20bb9e13a1cc87b1f3d4acfcb0d493b4e095
romaniv2511 <romaniv2511@gmail.com>
2022-11-14 22:35:45 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import user from "../user.json"
 import data from "../data.json"
 import friends from "../friends.json"
 import transactions from "../transactions.json"
-import { Profile } from "components/Profile/Profile";
-import { Statistics } from "components/Statistics/Statistics";
-import { FriendList } from "components/FriendList/FriendList";
-import { TransactionHistory } from "components/TransactionHistory/TransactionHistory";
+import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
 export const App = () => {
   return (
