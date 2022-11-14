40274f9e5af29eb2cd5110581b9eb9c82a541cbe
romaniv2511 <romaniv2511@gmail.com>
2022-11-14 19:07:11 +0200

    add transactions component add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import user from "../user.json"
 import data from "../data.json"
 import friends from "../friends.json"
+import transactions from "../transactions.json"
 import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
 export const App = () => {
   return (
@@ -18,7 +20,8 @@ export const App = () => {
       <Statistics title="Upload stats" data={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory transactions={transactions} />
     </>
   )
-};
-
+}
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,26 @@
+import "./transactionsHistory.css"
+
+export const TransactionHistory = ({ transactions }) => {
+    console.log(transactions);
+    return (
+        <table className="transactionTable">
+            <thead>
+                <tr>
+                <th className="transactionHeadItem">Type</th>
+                <th className="transactionHeadItem">Amount</th>
+                <th className="transactionHeadItem">Currency</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {transactions.map(({ id, type, amount, currency }) => (
+                <tr key={id} className="TransactionBodyRow">
+                    <td className="transactionBodyItem">{type}</td>
+                    <td className="transactionBodyItem">{amount}</td>
+                    <td className="transactionBodyItem">{currency}</td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
