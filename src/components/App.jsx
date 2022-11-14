cdfb1ee984efd6032a99971b09191ffb2523f5c4
romaniv2511 <romaniv2511@gmail.com>
2022-11-14 21:32:25 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import user from "../user.json"
 import data from "../data.json"
 import friends from "../friends.json"
 import transactions from "../transactions.json"
-import { Profile } from "./Profile/Profile";
+import { Profile } from "./Profile/Profile.jsx";
 import { Statistics } from "./Statistics/Statistics.jsx";
 import { FriendList } from "./FriendList/FriendList.jsx";
 import { TransactionHistory } from "./TransactionHistory/TransactionHistory.jsx";
