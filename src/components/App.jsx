c4b55803ace9a33bd9443096395fbf854d65276e
romaniv2511 <romaniv2511@gmail.com>
2022-11-14 21:38:48 +0200

    fix deployed bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import user from "../user.json"
 import data from "../data.json"
 import friends from "../friends.json"
 import transactions from "../transactions.json"
-import { Profile } from "./Profile/Profile.jsx";
-import { Statistics } from "./Statistics/Statistics.jsx";
-import { FriendList } from "./FriendList/FriendList.jsx";
-import { TransactionHistory } from "./TransactionHistory/TransactionHistory.jsx";
+import { Profile } from "components/Profile/Profile";
+import { Statistics } from "components/Statistics/Statistics";
+import { FriendList } from "components/FriendList/FriendList";
+import { TransactionHistory } from "components/TransactionHistory/TransactionHistory";
 
 export const App = () => {
   return (
